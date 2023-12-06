2973d3422c9c3941605a74494ad17f22573a2173
cytocracy <theop.parker@gmail.com>
2023-12-05 23:11:24 -0800

    day 5

--- 05.py
+++ 05.py
@@ -7,10 +7,10 @@ with open('inputs/05.txt') as f:
 
 seeds = lines[0].split(": ")[1].split(" ")
 
+PART = 2
 
-
-print(seeds)
-print(lines)
+# print(seeds)
+# print(lines)
 seedtosoilindex = lines.index("seed-to-soil map:") + 1
 seedtosoilmap = []
 
@@ -22,7 +22,7 @@ def mapthing(inp, triples):
 
 
 for i in range(seedtosoilindex, len(lines)):
-    print(i)
+    # print(i)
     if lines[i] == "":
         break
     else:
@@ -131,7 +131,7 @@ def getseedtolocation(seed):
 
     return location
 
-min = 9999999999999
+champmin = 999999999999
 seednums = []
 seedranges = []
 for (i, seed) in enumerate(seeds):
@@ -143,17 +143,161 @@ for (i, seed) in enumerate(seeds):
 
 #find min value of values of humitidy to location
 
+def getrange(triple):
+    dest, src, rge = triple
+    return (src, src + rge-1, dest-src)
+
+def fillrange(sortedranges, maxend):
+    sortedranges = sorted(sortedranges, key=lambda x: x[0])
+    for i in range(len(sortedranges)-1):
+        if sortedranges[i][1] + 1 < sortedranges[i+1][0]:
+            sortedranges.insert(i+1, (sortedranges[i][1] + 1, sortedranges[i+1][0] - 1, 0))
+    if sortedranges[0][0] != 0:
+        sortedranges.insert(0, (0, sortedranges[0][0] - 1, 0))
+    if sortedranges[-1][1] != maxend:
+        sortedranges.append((sortedranges[-1][1] + 1, maxend, 0))
+    return sortedranges
 
 
-dp = []
-for (i, seed) in enumerate(seednums):
-    for j in range(seednums[i], seednums[i] + seedranges[i]):
-        if j < len(dp) and dp[j] != None:
-            continue
-        if getseedtolocation(j) < min:
-            min = getseedtolocation(j)
-        dp.insert(j, getseedtolocation(j))
-    # if getseedtolocation(int(seed)) < min:
-    #     min = getseedtolocation(int(seed))
 
-print(min)
+
+def overlap(range1, range2):
+    start1, end1, val1 = range1
+    start2, end2, val2 = range2
+    if end1 < start2:
+        return False
+    if start1 > end2:
+        return False
+    return True
+
+def convert(triple):
+    low, high, add = triple
+    return (low+add, high+add)
+
+def mutate(seeds, newranges):
+    # print('newranges: ', newranges)
+    i = 0
+    untested = seeds.copy()
+    newseeds = []
+    while len(untested) > 0:
+        # seed = seeds.pop(0)
+        seed = untested.pop(0)
+        # j = 0
+        replaceranges = []
+        # untested = [seeds[i]]
+        j=0
+        # untested = [seeds[i]]
+        while j < len(newranges):
+            
+            if overlap((seed[0], seed[1], 0), newranges[j]):
+                print('overlap: ', seed, newranges[j])
+                if newranges[j][0] <= seed[0] and newranges[j][1] >= seed[1]: #all overlap
+                    newseeds.append(convert((seed[0], seed[1], newranges[j][2])))
+                    
+                    # print('append1')
+                elif newranges[j][0] > seed[0] and newranges[j][1] >= seed[1]: # right overlap
+                    newseeds.append(convert((newranges[j][0], seed[1], newranges[j][2])))
+                    untested.append((seed[0], newranges[j][0]-1, 0))
+                    
+                    # seed = (seed[0], newranges[j][0]-1)
+                    # replaceranges.append((seed[0], newranges[j][0]-1, 0))
+                    # print('append2')
+                
+                elif newranges[j][0] <= seed[0] and newranges[j][1] < seed[1]: #left overlap
+                    newseeds.append(convert((seed[0], newranges[j][1], newranges[j][2])))
+                    
+                    ##ONLY IF OTHERS DON'T OVERLAP TOO
+                    #HOW TO FIX?
+                    
+
+                    untested.append((newranges[j][1]+1, seed[1], 0))
+                    
+                    # seed = (newranges[j][1]+1, seed[1])
+                    # replaceranges.append((newranges[j][1]+1, seed[1], 0))
+                    # print('append3')
+                
+                else: # middle overlap
+                    newseeds.append(convert((newranges[j][0], newranges[j][1], newranges[j][2])))
+                    untested.append((seed[0], newranges[j][1]-1, 0))
+                    untested.append((newranges[j][0]+1, seed[1], 0))
+                    # seed = (seed[0], newranges[j][1]-1)
+                    # seeds.append((newranges[j][0]+1, seed[1]))
+                    # print('append4')
+                # print('append5')
+                break
+            
+            j += 1
+        
+        
+        # seeds.pop(i)
+        # if len(replaceranges) == 0:
+        #     replaceranges.append((seed[0], seed[1], 0))
+        # replaceranges = [(x+add, y+add) for (x, y, add) in replaceranges]
+        # # print('replaceranges: ', replaceranges)
+        # # print('adding in: ', replaceranges)
+        # seeds = seeds + replaceranges
+        # numskip = len(replaceranges)
+        # seeds = sorted(seeds, key=lambda x: x[0])
+        # i += numskip
+        # # print('i: ', i)
+    return newseeds
+
+
+if PART == 1:
+    pass
+elif PART == 2:
+    seedmapranges = [getrange(triple) for triple in seedtosoilmap]
+    soilranges = [getrange(triple) for triple in soiltofertilizermap]
+    fertilizerranges = [getrange(triple) for triple in ferttowatermap]
+    waterranges = [getrange(triple) for triple in watertolightmap]
+    lightranges = [getrange(triple) for triple in lighttotempmap]
+    tempranges = [getrange(triple) for triple in temptohumiditymap]
+    humidityranges = [getrange(triple) for triple in humiditytolocationmap]
+    
+   
+
+    # allranges= [seedmapranges, soilranges, fertilizerranges, waterranges, lightranges, tempranges, humidityranges]
+    # allranges = [sorted(ranges, key=lambda x: x[0]) for ranges in allranges]
+    # print('allranges\n', allranges)
+    maxend = 999999999999
+    
+    # allranges = [fillrange(ranges, maxend) for ranges in allranges]
+
+
+
+
+    initseeds = []
+    for i in range(len(seednums)):
+        initseeds.append((seednums[i], seednums[i] + seedranges[i]-1))
+    initseeds = sorted(initseeds, key=lambda x: x[0])
+
+    print('seedmapranges\n', fillrange(seedmapranges, maxend))
+    print(initseeds)
+
+
+    initseeds = mutate(initseeds, fillrange(seedmapranges, maxend))
+    print("1")
+    initseeds = mutate(initseeds, fillrange(soilranges, maxend))
+    print(initseeds)
+    print("2")
+    initseeds = mutate(initseeds, fillrange(fertilizerranges, maxend))
+    print(initseeds)
+    print("3")
+    initseeds = mutate(initseeds, fillrange(waterranges, maxend))
+    print(initseeds)
+    print("4")
+    initseeds = mutate(initseeds, fillrange(lightranges, maxend))
+    print(initseeds)
+    print("5")
+    initseeds = mutate(initseeds, fillrange(tempranges, maxend))
+    print(initseeds)
+    print("6")
+    initseeds = mutate(initseeds, fillrange(humidityranges, maxend))
+    print(initseeds)
+
+  
+    initseeds = sorted(initseeds, key=lambda x: x[0])
+    minloc = initseeds[0][0]
+    
+    print('minloc: ', minloc)
+
